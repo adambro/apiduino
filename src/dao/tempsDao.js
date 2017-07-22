1a241b7e86148db1fcebc4db8380ab33712c23f3
Daniel Kawka <ileev@hotmail.com>
2017-07-22 12:36:02 +0200

    Add timestamps for mongo

--- src/dao/tempsDao.js
+++ src/dao/tempsDao.js
@@ -2,7 +2,10 @@ const { mongoose } = require('./daoInit');
 
 const Schema = mongoose.Schema;
 
-const Temp = mongoose.model('temp', new Schema({}, { strict: false }));
+const Temp = mongoose.model('temp', new Schema({}, {
+  strict: false,
+  timestamps: true,
+}));
 
 const daoCreate = async payload => Temp.create(payload);
 
